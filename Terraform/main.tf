1dd17926d0ebf4785e5ffcd0bbfbc8f59f105802
Sorin Suvac <suvacsorin@gmail.com>
2024-10-29 21:48:55 +0200

    Move EC2 instances to VPC

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -10,8 +10,8 @@ terraform {
 }
 
 provider "aws" {
-  profile = "default"
-  region  = "eu-central-1"
+  profile                  = "default"
+  region                   = "eu-central-1"
   shared_credentials_files = ["/mnt/c/Users/ssuva/.aws/credentials"]
 }
 
@@ -20,8 +20,8 @@ resource "tls_private_key" "ssh_key" {
   rsa_bits  = 2048
 }
 
-resource "local_file" "private_key" {
-  content = tls_private_key.ssh_key.private_key_pem
+resource "local_file" "ssh_key_file" {
+  content  = tls_private_key.ssh_key.private_key_pem
   filename = "private-key.pem"
 }
 
@@ -29,58 +29,103 @@ resource "aws_key_pair" "ssh_key_pair" {
   public_key = tls_private_key.ssh_key.public_key_openssh
 }
 
-resource "aws_security_group" "ssh_group" {
-  name = "allow_ssh"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "route_table_association" {
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public.id
+}
+
+resource "aws_security_group" "allow_ssh_http_https" {
+  vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh-in_all-out"
   }
 }
 
 resource "aws_instance" "master" {
-  ami           = var.ubuntu_ami
-  instance_type = var.ec2_master_instance_type
-  key_name = aws_key_pair.ssh_key_pair.key_name
+  ami                         = var.ubuntu_ami
+  instance_type               = var.ec2_master_instance_type
+  subnet_id                   = aws_subnet.public.id
+  security_groups             = [aws_security_group.allow_ssh_http_https.id]
+  key_name                    = aws_key_pair.ssh_key_pair.key_name
+  associate_public_ip_address = true
 
   tags = {
     Name = "K8S-Master"
   }
-  vpc_security_group_ids = [aws_security_group.ssh_group.id]
 }
 
 output "master_ip" {
   value = aws_instance.master.public_ip
 }
 
-resource "aws_instance" "worker1" {
-  ami           = var.ubuntu_ami
-  instance_type = var.ec2_worker_instance_type
-  key_name = aws_key_pair.ssh_key_pair.key_name
+resource "aws_instance" "worker" {
+  count                       = 2
+  ami                         = var.ubuntu_ami
+  instance_type               = var.ec2_worker_instance_type
+  subnet_id                   = aws_subnet.public.id
+  security_groups             = [aws_security_group.allow_ssh_http_https.id]
+  key_name                    = aws_key_pair.ssh_key_pair.key_name
+  associate_public_ip_address = true
 
   tags = {
-    Name = "K8S-Worker1"
+    Name = "K8S-Worker${count.index + 1}"
   }
-  vpc_security_group_ids = [aws_security_group.ssh_group.id]
 }
 
-output "worker1_ip" {
-  value = aws_instance.worker1.public_ip
-}
-
-resource "aws_instance" "worker2" {
-  ami           = var.ubuntu_ami
-  instance_type = var.ec2_worker_instance_type
-  key_name = aws_key_pair.ssh_key_pair.key_name
-
-  tags = {
-    Name = "K8S-Worker2"
-  }
-  vpc_security_group_ids = [aws_security_group.ssh_group.id]
-}
-
-output "worker2_ip" {
-  value = aws_instance.worker2.public_ip
+output "worker_ips" {
+  value = aws_instance.worker[*].public_ip
 }
--- Terraform/vars.tf
+++ Terraform/vars.tf
@@ -1,17 +1,17 @@
 variable "ubuntu_ami" {
   description = "EC2 AMI Ubuntu 24.4"
-  type = string
-  default = "ami-0e872aee57663ae2d"
+  type        = string
+  default     = "ami-0e872aee57663ae2d"
 }
 
 variable "ec2_master_instance_type" {
   description = "EC2 Instance type of master nodes"
-  type = string
-  default = "t2.micro"
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "ec2_worker_instance_type" {
   description = "EC2 Instance type of worker nodes"
-  type = string
-  default = "t2.micro"
+  type        = string
+  default     = "t2.micro"
 }
